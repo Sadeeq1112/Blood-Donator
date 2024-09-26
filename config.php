71510e55065d44c336f7850430f6091585bda059
Sadeeq1112 <2019ns0974@unijos.edu.ng>
2024-09-26 22:14:52 +0100

    few changes

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'your_username');
-define('DB_PASSWORD', 'your_password');
-define('DB_NAME', 'blood_donation_db');
+define('DB_USERNAME', 'bfsllkdc_sadeeq');
+define('DB_PASSWORD', '09030037973Ab,');
+define('DB_NAME', 'bfsllkdc_blood');
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
